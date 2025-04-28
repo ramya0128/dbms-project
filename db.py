5ac2ebd2c55eab50a1d9510554371384a19bed78
ramya0128 <ramyaoli28@gmail.com>
2025-04-28 11:49:47 +0530

    Update db.py

--- db.py
+++ db.py
@@ -6,5 +6,8 @@ def init_db(app):
     mysql.init_app(app)
 
 def get_db():
-    return mysql.connection
+    conn = mysql.connection
+    conn.autocommit(False)  # Force manual commit
+    return conn
+
 
